5f69595ec84b19a41ca0a232908997e28085cb55
Ahmed Rabbi <ahmedrabbi.ff@gmail.com>
2024-08-13 05:51:25 +0000

    Fixed type for url param -claude.ai

--- components/common/Picture.tsx
+++ components/common/Picture.tsx
@@ -13,13 +13,13 @@ const isAbsoluteOrRootRelative = (url: string) =>
     /^(?:[a-z]+:)?\/\/|^\//i.test(url);
 
 export default function Picture({ src, alt, isPriority, className }: Props) {
-    const source = isAbsoluteOrRootRelative(src) ? src : "/prev.jpg";
     const imageSrc = typeof src === 'string' ? src : src.toString();
+    const source = isAbsoluteOrRootRelative(imageSrc) ? imageSrc : "/prev.jpg";
 
     return (
         <Image
             priority={isPriority}
-            src={imageSrc}
+            src={source}
             fill
             sizes="(min-width: 475px) 100vw, (min-width: 768px) 1080px, (min-width: 1024px) 1920px"
             alt={alt}
